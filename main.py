60dbcef80ad1c4555345315fd78f3301280d2b3e
Asi <71922418+AsiiiDK@users.noreply.github.com>
2022-07-26 21:51:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# Multiple line function
-def validPass(password):
-    if len(password) >= 8:
-        if any(char.isdigit() for char in password):
-            return "Valid"
-        else:
-            return "not valid"
-    else:
-        return "Not Valid"
-
-
 # One line function
 def validPass2(password):
     return "Valid" if len(password) >= 8 and any(char.isdigit() for char in password) else "Not valid"
